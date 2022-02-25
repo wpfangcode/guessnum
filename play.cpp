02f201bc98b5d9dfb6a96263594e4e3db3416bb3
s1102004 <96213778+s1102004@users.noreply.github.com>
2022-02-25 14:12:15 +0800

    1102004 江芸安

--- play.cpp
+++ play.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
+void split(int n, int N[])
+{
+	N[0] = n / 1000;
+	N[1] = (n / 100) % 10;
+	N[2] = (n / 10) % 10; 
+	N[3] = n % 10;
+}
+
+bool hasZero(int n)
+{
+	int N[4];
+	split(n, N);
+	/*
+	if (N[0] * N[1] * N[2] * N[3] = 0)
+		return true;
+	else
+		return false;
+	*/
+	return!(N[0] && N[1] && N[2] && N[3]);
+}
+
+void testcase1()
+{
+	int N[4];
+	split(3456, N);
+	cout << N[0] << " " << N[1] << " " << N[2] << " " << N[3] << endl;
+}
+
+void testcase2()
+{
+	if (hasZero(1023) == true)cout << "1023 has zero" << endl;
+	if (hasZero(1523) == false)cout << "1523 no zero" << endl;
+}
+
 int main()
 {
-  cout<<"student id"<<endl;
-  cout<<"name"<<endl;
-  return 1;
+  cout<<"1102004"<<endl;
+  cout<<"江芸安"<<endl;
+	testcase1();
+	testcase2();
 }
