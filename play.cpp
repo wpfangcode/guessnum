1bfc0a8d3f642e9a9997d8dbb74060b2a8abf9e2
steven5327 <96217393+steven5327@users.noreply.github.com>
2022-02-25 19:11:14 +0800

    s1102013 蕭家證

--- play.cpp
+++ play.cpp
@@ -1,10 +1,69 @@
 #include <iostream>
 using namespace std;
+
+void split(int n, int four[])
+{
+    four[0] = n / 1000;
+    four[1] = (n / 100) % 10;
+    four[2] = (n / 10) % 10;
+    four[3] = n % 10;
+}
+bool noZero(int n)
+{
+    int four[4];
+    split(n, four);
+    if ((four[0]) && (four[1]) && (four[2]) && (four[3]) != 0)
+        return true;
+    else
+        return false;
+}
+bool noRepeat(int n)
+{
+    int four[4];
+    split(n, four);
+    if ((four[0] != four[1]) && (four[0] != four[2]) && (four[0] != four[3]) && (four[1] != four[2]) && (four[1] != four[3]) && (four[2] != four[3]))
+        return true;
+    else
+        return false;
+
+}
+
 int main()
 {
-  cout<<"student id"<<endl;
-  cout<<"name"<<endl;
-  //-----------------------
-  cout<<"demo"<<endl;
-  return 1;
+  cout<<"s1102013"<<endl;
+  cout<<"蕭家證"<<endl;
+	int a = 0;
+	int b = 0;
+    int allnumber[3024] = { 0 };
+    int i;
+    int guess;
+
+    int number = 1234;
+    for(i=0;number<=9876;number++)
+    {
+        if (noZero(number) && noRepeat(number) )
+        {
+            allnumber[i]=number;
+                i++;
+        }
+    }
+    while (a != 4)
+    {
+        for (i = 0; i < 3024; i++)
+        {
+            if (allnumber[i] != -1)
+            {
+                guess = allnumber[i];
+                cout << "電腦猜:" << guess << endl;
+                allnumber[i] = -1;
+                break;
+            }
+        }
+        cout << "請輸入數字與AB" << endl;
+        cout << "A:";
+        cin >> a;
+        cout << "B:";
+        cin >> b;
+    }
+
 }
