6f3819e11a8e3d8528e481f82140c178fb629b43
YLChen1024 <96213724+YLChen1024@users.noreply.github.com>
2022-02-25 15:47:46 +0800

    1102034陳語翎

--- play.cpp
+++ play.cpp
@@ -2,9 +2,51 @@
 using namespace std;
 int main()
 {
-  cout<<"student id"<<endl;
-  cout<<"name"<<endl;
-  //-----------------------
-  cout<<"demo"<<endl;
-  return 1;
+    cout<<"1102034"<<endl;
+    cout<<"陳語翎"<<endl;
+    int a;
+    cin>>a;
+    int b;
+
+    while(cin>>b)
+    {
+        int c=0;
+        int d=0;
+        if(b==0000)
+            break;
+        if(a/1000==b/1000)
+            c=c+1;
+        if((a%1000)/100==(b%1000)/100)
+            c=c+1;
+        if((a%100)/10==(b%100)/10)
+            c=c+1;
+        if(a%10==b%10)
+            c=c+1;
+        if(a/1000==(b%1000)/100)
+            d=d+1;
+        if(a/1000==(b%100)/10)
+            d=d+1;
+        if(a/1000==b%10)
+            d=d+1;
+        if((a%1000)/100==b/1000)
+            d=d+1;
+        if((a%1000)/100==(b%100)/10)
+            d=d+1;
+        if((a%1000)/100==b%10)
+            d=d+1;
+        if((a%100)/10==b/1000)
+            d=d+1;
+        if((a%100)/10==(b%1000)/100)
+            d=d+1;
+        if((a%100)/10==b%10)
+            d=d+1;
+        if(a%10==b/1000)
+            d=d+1;
+        if(a%10==(b%1000)/100)
+            d=d+1;
+        if(a%10==(b%100)/10)
+            d=d+1;
+        cout<<c<<"A"<<d<<"B"<<endl;
+    }
+    return 0;
 }
