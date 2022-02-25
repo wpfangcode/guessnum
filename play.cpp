c1b1f299449c004fae39b2d1c9b55fd91b77d587
1102035 <96217276+1102035@users.noreply.github.com>
2022-02-25 13:40:23 +0800

    1102035袁萱芳

--- play.cpp
+++ play.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
 using namespace std;
+void spilt(int n, int N[])
+{
+	N[0] = n / 1000;
+	N[1] = (n / 100) % 10;
+	N[2] = (n / 10) % 10;
+	N[3] = n % 10;
+}
+bool nozero(int n)
+{
+	int N[4];
+	spilt(n, N);
+	if (N[0] * N[1] * N[2] * N[3] == 0)
+		return false;
+	else return true;
+}
+bool norepeat(int n)
+{
+	int N[4];
+	spilt(n, N);
+	if (N[0] == N[1])
+		return false;
+	else if (N[0] == N[2])
+		return false;
+	else if (N[0] == N[3])
+		return false;
+	else if (N[1] == N[2])
+		return false;
+	else if (N[1] == N[3])
+		return false;
+	else if (N[2] == N[3])
+		return false;
+	else
+		return true;
+}
+
 int main()
 {
-  cout<<"student id"<<endl;
-  cout<<"name"<<endl;
-  return 1;
+  cout<<"1102035"<<endl;
+  cout<<"袁萱芳"<<endl;
+  int N[3024] = { 0 };
+	int a = 0, b = 0;
+	int n = 1234;
+	for (int i = 0; n <= 9876; n++)
+	{
+		if (nozero(n) && norepeat(n))
+		{
+			N[i] = n;
+			i++;
+		}
+	}
+	int guess = 0;
+	while (a != 4)
+	{
+		for (int i = 0; i < 3024; i++)
+		{
+			if (N[i] != 0)
+			{
+				guess = N[i];
+				cout << "Answer:" << guess << endl;
+				N[i] = 0;
+				break;
+			}
+		}
+		cout << "A:";
+		cin >> a;
+		cout << "B:";
+		cin >> b;
+		cout<< endl;
+	}
+	cout << "The Final Answer is:" << guess << endl;
+	return 0;
 }
