153895439f96b78302314d11d7df22eded892390
s1072011 <46060534+s1072011@users.noreply.github.com>
2022-03-10 21:53:41 +0800

    1072011 楊哲其
    The function "Think()" has been optimized in class "Guess".

--- play.cpp
+++ play.cpp
@@ -21,311 +21,50 @@ public:
 	}
 	//移除不可能的答案
 	void Think(int a, int b) {
+		if (a > 4 || b > 4 || a < 0 || b < 0 || a + b > 4 || a + b < 0) {
+			cout << "輸入錯誤，請重新輸入" << endl;
+			inputFailed = true;
+			return;
+		}
+		if (a == 4) {
+			isOver = true;
+			cout << "嘿嘿 Bingo 答對囉" << endl;
+			return;
+		}
+		int i, j, k;
 		int _ans[4];
 		int _data[4];
-		int count = 0;
-		int same = 0;
-		int check = 0;
-		temp = new int[size];
+		int newSize = 0;
+		int* temp = new int[size];
 		Spilt(ans, _ans);
-		for (int i = 0; i < size; i++) {
-			Spilt(data[i], _data);
-			if (a == 0 && b == 0) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							same++;
+		for (k = 0; k < size; k++) {
+			int A = 0;
+			int B = 0;
+			Spilt(data[k], _data);
+			for (i = 0; i < 4; i++) {
+				for (j = 0; j < 4; j++) {
+					if (_data[i] == _ans[j]) {
+						if (i != j) {
+							B++;
+							break;
+						}
+						else {
+							A++;
 							break;
 						}
 					}
-					if (same)	break;
-				}
-				if (!same) {
-					temp[count] = data[i];
-					count++;
 				}
 			}
-			else if (a == 1 && b == 0) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								check++;
-								break;
-							}
-							else {
-								same++;
-								break;
-							}
-							break;
-						}
-					}
-					if (same > 1)break;
-				}
-				if (same == 1 && check == 1) {
-					temp[count] = data[i];
-					count++;
-				}
+			if (A == a && B == b) {
+				temp[newSize] = data[k];
+				newSize++;
 			}
-			else if (a == 0 && b == 1) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								check++;
-								break;
-							}
-							else {
-								same++;
-								break;
-							}
-							break;
-						}
-					}
-					if (same > 1 || check)break;
-				}
-				if (same == 1 && !check) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 2 && b == 0) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								check++;
-								break;
-							}
-							else {
-								same++;
-								break;
-							}
-							if (same > 1) break;
-						}
-					}
-					if (same > 2)break;
-				}
-				if (same == 2 && check == 2) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 0 && b == 2) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								break;
-							}
-							else {
-								check++;
-								break;
-							}
-							if (same > 0) break;
-						}
-					}
-					if (same > 0 || check > 2)break;
-				}
-				if (same == 0 && check == 2) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 1 && b == 1) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								break;
-							}
-							else {
-								check++;
-								break;
-							}
-							if (same > 1 || check > 1) break;
-						}
-					}
-					if (same > 1 || check > 1)break;
-				}
-				if (same == 1 && check == 1) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 3 && b == 0) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								check++;
-								break;
-							}
-							else {
-								same++;
-								break;
-							}
-							if (same > 3) break;
-						}
-					}
-					if (same > 3)break;
-				}
-				if (same == 3 && check == 3) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 0 && b == 3) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								break;
-							}
-							else {
-								check++;
-								break;
-							}
-							if (same > 0) break;
-						}
-					}
-					if (same > 0 || check > 3)break;
-				}
-				if (same == 0 && check == 3) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 2 && b == 1) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								break;
-							}
-							else {
-								check++;
-								break;
-							}
-							if (same > 2 || check > 1) break;
-						}
-					}
-					if (same > 2 || check > 1)break;
-				}
-				if (same == 2 && check == 1) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 1 && b == 2) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-								break;
-							}
-							else {
-								check++;
-								break;
-							}
-							if (same > 1 || check > 2) break;
-						}
-					}
-					if (same > 1 || check > 2)break;
-				}
-				if (same == 1 && check == 2) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 4 && b == 0) {
-				isOver = true;
-				cout << "嘿嘿 Bingo 答對囉" << endl;
-				return;
-			}
-			else if (a == 0 && b == 4) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-							}
-							else {
-								check++;
-							}
-							if (same > 0) break;
-						}
-					}
-					if (same > 0)break;
-				}
-				if (same == 0 && check == 4) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 1 && b == 3) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-							}
-							else {
-								check++;
-							}
-							if (same > 1 || check > 3) break;
-						}
-					}
-					if (same > 1 || check > 3)break;
-				}
-				if (same == 1 && check == 3) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else if (a == 2 && b == 2) {
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j < 4; j++) {
-						if (_data[i] == _ans[j]) {
-							if (i == j) {
-								same++;
-							}
-							else {
-								check++;
-							}
-							if (same > 2 || check > 2) break;
-						}
-					}
-					if (same > 2 || check > 2)break;
-				}
-				if (same == 2 && check == 2) {
-					temp[count] = data[i];
-					count++;
-				}
-			}
-			else {
-				cout << "輸入錯誤，請重新輸入" << endl;
-				inputFailed = true;
-				return;
-			}
-			same = 0;
-			check = 0;
 		}
-		size = count;
+		size = newSize;
 		delete[] data;
 		data = new int[size];
-		for (int i = 0; i < size; i++) {
+		for (i = 0; i < size; i++)
 			data[i] = temp[i];
-		}
 		delete[] temp;
 		round++;
 	}
@@ -353,7 +92,6 @@ private:
 	int lastRand = 0;
 	int round = 1;
 	int* data = new int[3024];
-	int* temp;
 	//檢查有沒有零
 	bool noZero(int num[]) {
 		return num[0] && num[1] && num[2] && num[3];
